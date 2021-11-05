585f8f5de2a803579e98772d404e2ed7739cd47a
Emmanuel Okoye <>
2021-11-05 16:36:28 +0100

    contract/id route fixed to return contract based on profile

--- src/app.js
+++ src/app.js
@@ -1,21 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
+const { sequelize } = require('./model');
+const { getProfile } = require('./middleware/getProfile');
 const app = express();
 app.use(bodyParser.json());
-app.set('sequelize', sequelize)
-app.set('models', sequelize.models)
+app.set('sequelize', sequelize);
+app.set('models', sequelize.models);
+const { Op } = require('sequelize');
 
 /**
  * FIX ME!
  * @returns contract by id
  */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
-    res.json(contract)
-})
+app.get('/contracts/:id', getProfile, async (req, res) => {
+    const { Contract, Profile } = req.app.get('models');
+    const { id: profileId } = req.profile;
+    const id = parseInt(req.params.id);
+    if (isNaN(id))
+        return res.status(400).end();
+
+    const contract = await Contract.findOne({
+        where: {
+            id: id,
+            [Op.or]: [
+                { ContractorId: profileId },
+                { ClientId: profileId }
+            ]
+        }
+    });
+
+    if (!contract) return res.status(404).end();
+    res.json(contract);
+});
 module.exports = app;
